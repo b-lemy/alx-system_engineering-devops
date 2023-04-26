0e992f35023a1aa2c1719df62d3af51045cc3a1f
b-lemy <lemabrian1234@gmail.com>
2023-04-26 07:48:09 -0700

    api corrections

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -19,4 +19,3 @@ if __name__ == '__main__':
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
     [print("\t {}".format(c)) for c in completed]
-
